75870998586edc44ca096f2f3ad12c030a6b443e
Romain Gilliotte <rgilliotte@zebestof.com>
2015-03-24 13:27:34 +0100

    Stop crashing on a race condition on load (by checking acls before the user is loaded).

--- client/dev/js/directives/acls.js
+++ client/dev/js/directives/acls.js
@@ -7,7 +7,6 @@
 // acl-fallback
 
 function _makeReadOnly(scope, element, attributes) {
-
 	// replace by raw text
 	if (attributes.aclFallback) {
 		var fallback = scope.$eval(attributes.aclFallback) || '';
@@ -37,74 +36,90 @@ angular.module('monitool.directives.acl', [])
 	.directive('aclHasRole', function() {
 		return {
 			link: function(scope, element, attributes) {
-				var roles = scope.userCtx.roles || [],
-					isAllowed = roles.indexOf(attributes.aclHasRole) !== -1 || roles.indexOf('_admin') !== -1;
+				var unwatch = scope.$watch('userCtx', function(userCtx) {
+					if (!userCtx)
+						return;
 
-				if (!isAllowed)
-					_makeReadOnly(scope, element, attributes);
+					var roles = userCtx.roles || [],
+						isAllowed = roles.indexOf(attributes.aclHasRole) !== -1 || roles.indexOf('_admin') !== -1;
+
+					if (!isAllowed)
+						_makeReadOnly(scope, element, attributes);
+
+					unwatch();
+				});
 			}
 		}
 	})
 	.directive('aclHasProjectRole', function() {
 		return {
 			link: function(scope, element, attributes) {
-				var roles = scope.userCtx.roles || [],
-					project = scope.$eval(attributes.project) || scope.project,
-					owners;
+				var unwatch = scope.$watch('userCtx', function(userCtx) {
+					if (!userCtx)
+						return;
 
+					var roles = userCtx.roles || [],
+						project = scope.$eval(attributes.project) || scope.project,
+						owners;
 
-				if (attributes.aclHasProjectRole === 'owner')
-					owners = project.owners || [];
-				else if (attributes.aclHasProjectRole === 'input')
-					owners = project.dataEntryOperators || [];
-				else
-					throw new Error("acl-has-project-role must be called with either 'owner' or 'input'");
+					if (attributes.aclHasProjectRole === 'owner')
+						owners = project.owners || [];
+					else if (attributes.aclHasProjectRole === 'input')
+						owners = project.dataEntryOperators || [];
+					else
+						throw new Error("acl-has-project-role must be called with either 'owner' or 'input'");
 
-				// if (!project._id)
-				// 	return roles.indexOf('project_create') !== -1 || roles.indexOf('_admin') !== -1;
-				// else {
-					var isAllowed = owners.indexOf(scope.userCtx._id) !== -1 || roles.indexOf('_admin') !== -1;
+					var isAllowed = owners.indexOf(userCtx._id) !== -1 || roles.indexOf('_admin') !== -1;
 					if (!isAllowed)
 						_makeReadOnly(scope, element, attributes);
-				// }
+
+					unwatch();
+				});
 			}
 		}
 	})
 	.directive('aclLacksRole', function() {
 		return {
 			link: function(scope, element, attributes) {
-				var roles = scope.userCtx.roles || [],
-					isForbidden = roles.indexOf(attributes.aclLacksRole) === -1 && roles.indexOf('_admin') === -1;
+				var unwatch = scope.$watch('userCtx', function(userCtx) {
+					if (!userCtx)
+						return;
 
-				if (!isForbidden)
-					_makeReadOnly(scope, element, attributes);
+					var roles = userCtx.roles || [],
+						isForbidden = roles.indexOf(attributes.aclLacksRole) === -1 && roles.indexOf('_admin') === -1;
+
+					if (!isForbidden)
+						_makeReadOnly(scope, element, attributes);
+
+					unwatch();
+				});
 			}
 		}
 	})
 	.directive('aclLacksProjectRole', function() {
 		return {
 			link: function(scope, element, attributes) {
-				var roles = scope.userCtx.roles || [],
-					project = scope.$eval(attributes.project) || scope.project,
-					owners;
+				var unwatch = scope.$watch('userCtx', function(userCtx) {
+					if (!userCtx)
+						return;
 
-					// console.log(scope.project)
+					var roles = userCtx.roles || [],
+						project = scope.$eval(attributes.project) || scope.project,
+						owners;
 
+					if (attributes.aclLacksProjectRole === 'owner')
+						owners = scope.project.owners || [];
+					else if (attributes.aclLacksProjectRole === 'input')
+						owners = scope.project.dataEntryOperators || [];
+					else
+						throw new Error("acl-lacks-project-role must be called with either 'owner' or 'input'");
 
-				if (attributes.aclLacksProjectRole === 'owner')
-					owners = scope.project.owners || [];
-				else if (attributes.aclLacksProjectRole === 'input')
-					owners = scope.project.dataEntryOperators || [];
-				else
-					throw new Error("acl-lacks-project-role must be called with either 'owner' or 'input'");
-
-				// if (!scope.project._id)
-				// 	return roles.indexOf('project_create') === -1 && roles.indexOf('_admin') === -1;
-				// else {
-					var isForbidden = owners.indexOf(scope.userCtx._id) === -1 && roles.indexOf('_admin') === -1;
+					var isForbidden = owners.indexOf(userCtx._id) === -1 && roles.indexOf('_admin') === -1;
 					if (!isForbidden)
 						_makeReadOnly(scope, element, attributes);
-				// }
+
+					unwatch();
+				});
 			}
 		}
 	});
