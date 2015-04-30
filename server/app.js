02758422a22743599aff4af66e904f3fdcb1ab41
Romain Gilliotte <rgilliotte@zebestof.com>
2015-04-30 10:49:52 +0200

    Catch uncaughtEcceptions to avoid having the server crashing and loosing all sessions when there is a problem

--- server/app.js
+++ server/app.js
@@ -61,3 +61,11 @@ if (config.ping && config.ping.active)
 			
 		});
 	}, 10000);
+
+
+// catch the uncaught errors that weren't wrapped in a domain or try catch statement
+// do not use this in modules, but only in applications, as otherwise we could have multiple of these bound
+process.on('uncaughtException', function(err) {
+    // handle the error safely
+    console.log(err.stack)
+});
