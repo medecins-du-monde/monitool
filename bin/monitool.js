7bb29dc55f28774498f48f009b7926fcd89f4de2
Romain Gilliotte <romain.gilliotte@medecinsdumonde.net>
2017-04-07 13:36:42 +0200

    Move session store to separate file.

--- bin/monitool.js
+++ bin/monitool.js
@@ -8,19 +8,8 @@ var express      = require('express'),
 	session      = require('express-session'),
 	path         = require('path'),
 	passport     = require('../server/authentication/passport'),
+	sessionStore = require('../server/authentication/session-store'),
 	config       = require('../server/config');
-
-var CouchSessionStore = require('connect-couchdb')(session),
-	store = new CouchSessionStore({
-		name: config.couchdb.sessionBucket,
-		host: config.couchdb.host,
-		port: config.couchdb.port,
-		username: config.couchdb.username,
-		password: config.couchdb.password
-	});
-
-
-
 
 express()
 	.disable('x-powered-by')
@@ -43,7 +32,7 @@ express()
 	
 	// Enable dynamic sessions and compression for the rest.
 	.use(cookieParser())
-	.use(session({ secret: 'cd818da5bcd0d3d9ba5a9a44e49148d2', resave: false, saveUninitialized: false, store: store }))
+	.use(session({ secret: 'cd818da5bcd0d3d9ba5a9a44e49148d2', resave: false, saveUninitialized: false, store: sessionStore }))
 	.use(passport.initialize())
 	.use(passport.session())
 	.use(compression())
---
+++ server/authentication/session-store.js
@@ -0,0 +1,45 @@
+/*!
+ * This file is part of Monitool.
+ *
+ * Monitool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Monitool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Monitool. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+let config         = require('../config'),
+	nano           = require('nano'),
+	session        = require('express-session')
+	connectCouchDB = require('connect-couchdb');
+
+let CouchSessionStore = connectCouchDB(session);
+
+let store = new CouchSessionStore({
+	name: config.couchdb.sessionBucket,
+	host: config.couchdb.host,
+	port: config.couchdb.port,
+	username: config.couchdb.username,
+	password: config.couchdb.password
+});
+
+
+// Create bucket if not existing already
+let url = 'http://';
+if (config.couchdb.username && config.couchdb.password)
+	url += config.couchdb.username + ':' + config.couchdb.password + '@';
+url += config.couchdb.host + ':' + config.couchdb.port;
+
+var nn = nano(url);
+nn.db.create(config.couchdb.sessionBucket, function(error) {
+});
+
+module.exports = store;
