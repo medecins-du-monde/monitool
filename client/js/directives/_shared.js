a9f5edb31a7acec0c4de5326b50c289788896703
Romain Gilliotte <romain.gilliotte@medecinsdumonde.net>
2017-04-07 16:17:11 +0200

    Remove dead code

--- client/js/directives/_shared.js
+++ client/js/directives/_shared.js
@@ -68,37 +68,3 @@ angular.module('monitool.directives.shared', [])
 			}
 		}
 	})
-
-	// .directive('eatClickIf', function($parse, $rootScope) {
-	// 	return {
-	// 	 	// this ensure eatClickIf be compiled before ngClick
-	// 		priority: 100,
-	// 		restrict: 'A',
-	// 		compile: function($element, attr) {
-	// 			var fn = $parse(attr.eatClickIf);
-	// 			return {
-	// 				pre: function link(scope, element) {
-	// 					var eventName = 'click';
-
-	// 					element.on(eventName, function(event) {
-	// 						var callback = function() {
-	// 							if (fn(scope, {$event: event})) {
-	// 								// prevents ng-click to be executed
-	// 								// prevents href 
-	// 								event.stopImmediatePropagation();
-	// 								event.preventDefault();
-	// 								return false;
-	// 							}
-	// 						};
-
-	// 						if ($rootScope.$$phase)
-	// 							scope.$evalAsync(callback);
-	// 						else
-	// 							scope.$apply(callback);
-	// 					});
-	// 				},
-	// 				post: function() {}
-	// 			};
-	// 		}
-	// 	}
-	// });
