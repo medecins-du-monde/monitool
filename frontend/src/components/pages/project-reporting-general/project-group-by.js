c2caa0e810169588662b1777ff235922864bb44c
Romain Gilliotte <romain.gilliotte@medecinsdumonde.net>
2018-08-01 11:36:28 +0200

    Fix default groupby

--- frontend/src/components/pages/project-reporting-general/project-group-by.js
+++ frontend/src/components/pages/project-reporting-general/project-group-by.js
@@ -56,9 +56,9 @@ module.component('projectGroupBy', {
 		_chooseDefaultGroupBy() {
 			let start = new Date(this.project.start + 'T00:00:00Z');
 			let end = new Date(this.project.end + 'T00:00:00Z');
-			let now = new Date();
-			if (now < end)
-				end = now;
+			// let now = new Date();
+			// if (now < end)
+			// 	end = now;
 
 			let chosen = this.periodicities[this.periodicities.length - 1];
 
