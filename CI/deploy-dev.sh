d5a48ad5d432f44a3c389bc41158bb843595b32c
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 12:34:03 +0100

    adding comments

--- CI/deploy-dev.sh
+++ CI/deploy-dev.sh
@@ -6,8 +6,10 @@ CONNECTION=reliefapps@92.243.25.191
 
 echo -e "Synchronizing files and restarting the docker ..."
 CMD="cd ${REMOTE_PATH}; \
+    echo -e 'checking out dev'; \
     git checkout dev; \
     git pull origin dev; \
+    echo -e 'running docker compose'; \
     sudo cp compose-develop.yml docker-compose.yml;\
     sudo docker-compose up --build\
     "
