f6b544696301d66b6528ff08e97052d8e3b38323
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 12:57:34 +0100

    changes in the deploy-dev.sh

--- CI/deploy-dev.sh
+++ CI/deploy-dev.sh
@@ -17,7 +17,7 @@ CMD="cd ${REMOTE_PATH}; \
     git checkout dev; \
     git pull origin dev; \
     echo -e 'running docker compose'; \
-    sudo cp compose-develop.yml docker-compose.yml;\
+    cp compose-develop.yml docker-compose.yml;\
     sudo docker-compose up --build\
     "
 ssh -i $1 $CONNECTION $CMD
