ed4d380c2d6e8bbecbd9540a46335b3f31412deb
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 10:20:21 +0100

    Travis integration

---
+++ CI/deploy-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REMOTE_PATH=/var/www/html/monitool
+
+CONNECTION=reliefapps@92.243.25.191
+
+echo -e "Synchronizing files and restarting the docker ..."
+CMD="cd ${REMOTE_PATH}; \
+    git checkout dev; \
+    git pull origin dev; \
+    sudo cp compose-develop.yml docker-compose.yml;\
+    sudo docker-compose up --build\
+    "
+ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "$CMD"
+
+echo -e "Deployed !!"
