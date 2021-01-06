7a4aa85042b526ae4c86501ea925390c8bfa5927
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 12:46:42 +0100

    some changes deply-dev.sh

--- CI/deploy-dev.sh
+++ CI/deploy-dev.sh
@@ -16,6 +16,6 @@ CMD="cd ${REMOTE_PATH}; \
     sudo cp compose-develop.yml docker-compose.yml;\
     sudo docker-compose up --build\
     "
-ssh -i $1 $CONNECTION "$CMD"
+ssh -i $1 $CONNECTION $CMD
 
 echo -e "Deployed !!!"
