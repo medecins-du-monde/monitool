c945862e051f9c75bda1f3e6a1ec40a29615b918
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 12:54:51 +0100

    adding ssh keyscan

--- CI/deploy-dev.sh
+++ CI/deploy-dev.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 REMOTE_PATH=/var/www/html/monitool
-
+IP=92.243.25.191
 CONNECTION=reliefapps@92.243.25.191
 
 #immediately exits if a command exits with an non-zero status
 set -e
+
+if [ -z `ssh-keygen -F $IP` ]; then
+        ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+fi
 
 echo -e "Synchronizing files and restarting the docker ..."
 CMD="cd ${REMOTE_PATH}; \
