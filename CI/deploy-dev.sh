7f8cf338dd1ca4fd0c54941ea573c1b750244aff
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-06 15:46:54 +0100

    changes in the travis.yml

--- CI/deploy-dev.sh
+++ CI/deploy-dev.sh
@@ -11,15 +11,17 @@ if [ -z `ssh-keygen -F $IP` ]; then
         ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
 
-echo -e "Synchronizing files and restarting the docker ..."
 CMD="cd $REMOTE_PATH; \
     echo -e 'checking out dev'; \
     git checkout dev; \
+    echo -e 'Stopping docker containers...'; \
+    echo $SSH_PASS | sudo -S docker-compose up --build -d;\
+    echo -e 'pulling dev...'; \
     git pull origin dev; \
     echo -e 'copying files'; \
     cd docker; \
     cp compose-develop.yml.dist docker-compose.yml;\
-    echo -e 'running docker compose'; \
+    echo -e 'Starting docker containers...'; \
     echo $SSH_PASS | sudo -S docker-compose up --build -d;\
     "
 ssh -i $1 $CONNECTION $CMD
