7afef1c4cac529712992d100aa3965b338b8756a
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-01-31 10:43:00 +0100

    fix urls

--- api/lib/passport.js
+++ api/lib/passport.js
@@ -92,8 +92,8 @@ passport.deserializeUser(function(id, done) {
 if (config.auth.providers.azureAD) {
 	var strategy = new OAuth2Strategy(
 		{
-			authorizationURL: "https://login.microsoftonline.com/" + config.auth.providers.azureAD.tenantId + "/oauth2/v2.0/authorize",
-			tokenURL: "https://login.microsoftonline.com/" + config.auth.providers.azureAD.tenantId + "/oauth2/v2.0/token",
+			authorizationURL: "https://login.microsoftonline.com/" + config.auth.providers.azureAD.tenantId + "/oauth2/authorize",
+			tokenURL: "https://login.microsoftonline.com/" + config.auth.providers.azureAD.tenantId + "/oauth2/token",
 			clientID: config.auth.providers.azureAD.clientId,
 			clientSecret: config.auth.providers.azureAD.clientSecret,
 			callbackURL: config.baseUrl + '/api/authentication/login-callback'
