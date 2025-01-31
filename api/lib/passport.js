513dcf878fdc3ae0dd635bbd8b3816cfb81644bc
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-01-31 09:35:06 +0100

    update strategy token params

--- api/lib/passport.js
+++ api/lib/passport.js
@@ -173,7 +173,7 @@ if (config.auth.providers.azureAD) {
 	//  in our case we're just trying to authenticate, so we just request generic access to the Azure AD graph API
 	strategy.tokenParams = strategy.authorizationParams = function(options) {
 		console.log('strategy.tokenParams:', 'Getting resource');
-		return { resource: "https://graph.windows.net" };
+		return { resource: "https://graph.microsoft.com" };
 	};
 	// this is our custom logic for digging into the token returned to us by Azure
 	//  in raw form its base64 text and we want the corresponding JSON
