4be3667b351bd46603f2686cdb3daa2110256cad
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-02-04 15:53:59 +0100

    Fix migration

--- api/lib/resource/migrations/migration-15.js
+++ api/lib/resource/migrations/migration-15.js
@@ -3,17 +3,6 @@ import database from '../database';
 
 // Add shortName to all themes.
 export default async () => {
-    // Update themes.
-    const result = await database.callList({ include_docs: true, startkey: 'theme:!', endkey: 'theme:~' });
-    const documents = result.rows.map(r => r.doc);
-
-    documents.forEach(doc => {
-        doc.shortName = {};
-        for (let key in doc.name)
-            doc.shortName[key] = doc.name[key].substr(0, 5);
-    });
-
-    await database.callBulk({ docs: documents });
 
     // Update design document.
     const ddoc = await database.get('_design/monitool');
